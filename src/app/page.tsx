c00effb9e18dd04bee67b3662c81f9eac8d52902
gurnit <gurjiitsingh@gmail.com>
2025-12-08 08:45:41 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-      
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center gap-5 py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <h1 className="text-5xl text-gray-800"> Masala Taste of India</h1>
         <Link
                 href="https://eat.allo.restaurant/restaurant/masala-taste-of-india"
                 rel="noopener noreferrer"
